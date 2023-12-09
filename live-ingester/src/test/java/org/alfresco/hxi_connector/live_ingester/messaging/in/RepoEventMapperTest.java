026e9c43a959d0922c89ed6e00d5aea24f17b2b1
Pawel Rainer <pawel.rainer@hyland.com>
2023-12-09 16:32:05 +0100

    ASC-6376: test

--- live-ingester/src/test/java/org/alfresco/hxi_connector/live_ingester/messaging/in/RepoEventMapperTest.java
+++ live-ingester/src/test/java/org/alfresco/hxi_connector/live_ingester/messaging/in/RepoEventMapperTest.java
@@ -52,7 +52,6 @@ import org.junit.jupiter.api.Test;
 
 class RepoEventMapperTest
 {
-
     private static final long EVENT_TIMESTAMP = 1690000000100L;
     private static final String NODE_ID = "0fe2919a-e0a6-4033-8d35-168a16cf33fc";
     private static final String NODE_NAME = "test-name";
