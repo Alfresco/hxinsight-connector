7707f51ecef2e7a9af795e37a46335b5a25294cd
Krystian Dabrowski <krystian.dabrowski@hyland.com>
2024-02-27 16:54:49 +0100

    ACS-6389: [HxI PH] Listen to predictions from HxI

--- distribution/src/main/resources/docker-compose/init-aws.sh
+++ distribution/src/main/resources/docker-compose/init-aws.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 awslocal s3api create-bucket --bucket "$BUCKET_NAME"
+awslocal sqs create-queue --queue-name "$SQS_QUEUE_NAME"
---
+++ prediction-applier/src/integration-test/java/org/alfresco/hxi_connector/prediction_applier/util/LoggingUtils.java
@@ -0,0 +1,64 @@
+/*
+ * #%L
+ * Alfresco HX Insight Connector
+ * %%
+ * Copyright (C) 2024 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.hxi_connector.prediction_applier.util;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.read.ListAppender;
+import org.slf4j.LoggerFactory;
+
+public class LoggingUtils
+{
+
+    public static ListAppender<ILoggingEvent> createLogsListAppender(Class<?> classToTrack, Class<?>... classesToTrack)
+    {
+        return createLogsAppender(ListAppender<ILoggingEvent>::new, Stream.concat(Stream.of(classToTrack), Arrays.stream(classesToTrack)).toArray(Class[]::new));
+    }
+
+    private static <T extends Appender<ILoggingEvent>> T createLogsAppender(Supplier<T> appenderSupplier, Class<?>... classesToTrack)
+    {
+        T appender = appenderSupplier.get();
+
+        Arrays.stream(classesToTrack).forEach(classToTrack -> addAppenderForLogger(appender, classToTrack));
+        appender.start();
+
+        return appender;
+    }
+
+    private static void addAppenderForLogger(Appender<ILoggingEvent> appender, Class<?> classToTrack)
+    {
+        Logger logger = (Logger) LoggerFactory.getLogger(classToTrack);
+
+        logger.addAppender(appender);
+        logger.setLevel(Level.DEBUG);
+    }
+}
