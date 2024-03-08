7cba998c118bc851065ccee22f98cbf5c8f7761a
Tom Page <tom.page@alfresco.com>
2024-03-08 15:04:11 +0000

    Try running pre-commit against modified files only.

--- scripts/hooks/license-check-and-update.sh
+++ scripts/hooks/license-check-and-update.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-modified_files=$(git diff --cached --name-only --diff-filter=AM)
+if [[ -e ${GITHUB_MODIFIED_FILES} ]]
+then
+  modified_files=${GITHUB_MODIFIED_FILES}
+else
+  modified_files=$(git diff --cached --name-only --diff-filter=ACMR)
+fi
 
-mvn validate -DlicenseUpdateHeaders=true > /dev/null
+mvn validate -DlicenseUpdateHeaders=true
 
-all_old_headers=$(git diff --name-only --diff-filter=AM)
+all_old_headers=$(git diff --name-only --diff-filter=ACMR)
 
 for file in ${all_old_headers}
 do
