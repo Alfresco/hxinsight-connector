2a0d72365a35f8d47f373695e78bf2a772e86306
mpichura <maciej.pichura@hyland.com>
2024-03-12 12:16:37 +0100

    ACS-6871: Adding Wire Mock resets and 200 ms sleep before each

--- live-ingester/src/integration-test/java/org/alfresco/hxi_connector/live_ingester/util/E2ETestBase.java
+++ live-ingester/src/integration-test/java/org/alfresco/hxi_connector/live_ingester/util/E2ETestBase.java
@@ -42,6 +42,7 @@ import static org.alfresco.hxi_connector.live_ingester.util.ContainerSupport.BUL
 import static org.alfresco.hxi_connector.live_ingester.util.ContainerSupport.REPO_EVENT_TOPIC;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.matching.EqualToPattern;
@@ -167,12 +168,15 @@ public class E2ETestBase
     public void setUp()
     {
         containerSupport = ContainerSupport.getInstance(hxInsightMock, brokerUrl, localStorageClient);
+        TimeUnit.MILLISECONDS.sleep(200);
     }
 
     @AfterEach
     public void reset()
     {
         WireMock.reset();
+        hxInsightMock.resetRequests();
+        sfsMock.resetRequests();
         containerSupport.clearATSQueue();
     }
 
