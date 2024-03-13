8d25bc2f2d999a0a33e25af7474aa77db8391f0e
mpichura <maciej.pichura@hyland.com>
2024-03-13 09:19:03 +0100

    ACS-6871: Fixing test requests.

--- live-ingester/src/integration-test/java/org/alfresco/hxi_connector/live_ingester/domain/usecase/e2e/repository/RequestFiltersIntegrationTest.java
+++ live-ingester/src/integration-test/java/org/alfresco/hxi_connector/live_ingester/domain/usecase/e2e/repository/RequestFiltersIntegrationTest.java
@@ -479,16 +479,16 @@ public class RequestFiltersIntegrationTest extends E2ETestBase
                 {
                   "specversion": "1.0",
                   "type": "org.alfresco.event.node.Created",
-                  "id": "368818d9-dddd-4b8b-8eab-e050253d7f03",
+                  "id": "368818d9-dddd-4b8b-8eab-e050253d7f07",
                   "source": "/08d9b620-48de-4247-8f33-360988d3b19b",
                   "time": "2021-01-21T11:14:16.42372Z",
                   "dataschema": "https://api.alfresco.com/schema/event/repo/v1/nodeCreated",
                   "datacontenttype": "application/json",
                   "data": {
-                    "eventGroupId": "4004ca99-9d2a-400d-9d80-8f840e223503",
+                    "eventGroupId": "4004ca99-9d2a-400d-9d80-8f840e223507",
                     "resource": {
                       "@type": "NodeResource",
-                      "id": "d71dd823-82c7-477c-8490-04cb0e826e03",
+                      "id": "d71dd823-82c7-477c-8490-04cb0e826e07",
                       "primaryHierarchy": [ "5f355d16-f824-4173-bf4b-b1ec37ef5549", "93f7edf5-e4d8-4749-9b4c-e45097e2e19d" ],
                       "name": "purchase-order-scan.pdf",
                       "nodeType": "cm:content",
@@ -537,16 +537,16 @@ public class RequestFiltersIntegrationTest extends E2ETestBase
                 {
                   "specversion": "1.0",
                   "type": "org.alfresco.event.node.Created",
-                  "id": "368818d9-dddd-4b8b-8eab-e050253d7f03",
+                  "id": "368818d9-dddd-4b8b-8eab-e050253d7f08",
                   "source": "/08d9b620-48de-4247-8f33-360988d3b19b",
                   "time": "2021-01-21T11:14:16.42372Z",
                   "dataschema": "https://api.alfresco.com/schema/event/repo/v1/nodeCreated",
                   "datacontenttype": "application/json",
                   "data": {
-                    "eventGroupId": "4004ca99-9d2a-400d-9d80-8f840e223503",
+                    "eventGroupId": "4004ca99-9d2a-400d-9d80-8f840e223508",
                     "resource": {
                       "@type": "NodeResource",
-                      "id": "d71dd823-82c7-477c-8490-04cb0e826e03",
+                      "id": "d71dd823-82c7-477c-8490-04cb0e826e08",
                       "primaryHierarchy": [ "5f355d16-f824-4173-bf4b-b1ec37ef5549", "93f7edf5-e4d8-4749-9b4c-e45097e2e19d" ],
                       "name": "purchase-order-scan.pdf",
                       "nodeType": "cm:content",
@@ -594,16 +594,16 @@ public class RequestFiltersIntegrationTest extends E2ETestBase
                 {
                   "specversion": "1.0",
                   "type": "org.alfresco.event.node.Created",
-                  "id": "368818d9-dddd-4b8b-8eab-e050253d7f01",
+                  "id": "368818d9-dddd-4b8b-8eab-e050253d7f09",
                   "source": "/08d9b620-48de-4247-8f33-360988d3b19b",
                   "time": "2021-01-21T11:14:16.42372Z",
                   "dataschema": "https://api.alfresco.com/schema/event/repo/v1/nodeCreated",
                   "datacontenttype": "application/json",
                   "data": {
-                    "eventGroupId": "4004ca99-9d2a-400d-9d80-8f840e223501",
+                    "eventGroupId": "4004ca99-9d2a-400d-9d80-8f840e223509",
                     "resource": {
                       "@type": "NodeResource",
-                      "id": "d71dd823-82c7-477c-8490-04cb0e826e01",
+                      "id": "d71dd823-82c7-477c-8490-04cb0e826e09",
                       "primaryHierarchy": [ "5f355d16-f824-4173-bf4b-b1ec37ef5549", "93f7edf5-e4d8-4749-9b4c-e45097e2e19d" ],
                       "name": "purchase-order-scan.pdf",
                       "nodeType": "cm:folder",
@@ -652,16 +652,16 @@ public class RequestFiltersIntegrationTest extends E2ETestBase
                 {
                   "specversion": "1.0",
                   "type": "org.alfresco.event.node.Updated",
-                  "id": "ae5dac3c-25d0-438d-b148-2084d1ab0504",
+                  "id": "ae5dac3c-25d0-438d-b148-2084d1ab0510",
                   "source": "/08d9b620-48de-4247-8f33-360988d3b19b",
                   "time": "2021-01-26T10:29:42.99524Z",
                   "dataschema": "https://api.alfresco.com/schema/event/repo/v1/nodeUpdated",
                   "datacontenttype": "application/json",
                   "data": {
-                    "eventGroupId": "b5b1ebfe-45fc-4f86-b71b-421996482804",
+                    "eventGroupId": "b5b1ebfe-45fc-4f86-b71b-421996482810",
                     "resource": {
                       "@type": "NodeResource",
-                      "id": "d71dd823-82c7-477c-8490-04cb0e826e04",
+                      "id": "d71dd823-82c7-477c-8490-04cb0e826e10",
                       "name": "purchase-order-scan.pdf",
                       "nodeType": "cm:folder",
                       "createdByUser": {
