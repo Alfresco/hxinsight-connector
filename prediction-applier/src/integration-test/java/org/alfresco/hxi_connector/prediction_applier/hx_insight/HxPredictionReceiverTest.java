abce331807eb60713d0e9c19459c34b65c1a416a
Pawel Rainer <pawel.rainer@hyland.com>
2024-04-18 21:53:19 +0200

    ACS-7544: pmd

--- prediction-applier/src/integration-test/java/org/alfresco/hxi_connector/prediction_applier/hx_insight/HxPredictionReceiverTest.java
+++ prediction-applier/src/integration-test/java/org/alfresco/hxi_connector/prediction_applier/hx_insight/HxPredictionReceiverTest.java
@@ -61,7 +61,7 @@ import org.alfresco.hxi_connector.prediction_applier.util.PredictionsTriggerStub
                 InternalPredictionBufferStub.class, PredictionSourceStub.class, PredictionsTriggerStub.class
         })
 @EnableAutoConfiguration
-@SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert", "PMD.LongVariable", "PMD.LinguisticNaming", "PMD.UnusedPrivateMethod"})
+@SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert", "PMD.LongVariable", "PMD.LinguisticNaming"})
 class HxPredictionReceiverTest
 {
     @Autowired
