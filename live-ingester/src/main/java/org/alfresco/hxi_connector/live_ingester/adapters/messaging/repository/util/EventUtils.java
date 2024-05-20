a64f08630b4a7e10d91fd7d00af48658d6931408
Pawel Rainer <pawel.rainer@hyland.com>
2024-05-20 15:13:45 +0200

    ACS-7708: send info to hxi about confirmed predictions

--- live-ingester/src/main/java/org/alfresco/hxi_connector/live_ingester/adapters/messaging/repository/util/EventUtils.java
+++ live-ingester/src/main/java/org/alfresco/hxi_connector/live_ingester/adapters/messaging/repository/util/EventUtils.java
@@ -26,6 +26,8 @@
 
 package org.alfresco.hxi_connector.live_ingester.adapters.messaging.repository.util;
 
+import static java.util.Optional.ofNullable;
+
 import static lombok.AccessLevel.PRIVATE;
 
 import static org.alfresco.hxi_connector.live_ingester.domain.usecase.metadata.model.EventType.CREATE;
@@ -46,6 +48,8 @@ import lombok.NoArgsConstructor;
 
 import org.alfresco.hxi_connector.live_ingester.domain.exception.LiveIngesterRuntimeException;
 import org.alfresco.hxi_connector.live_ingester.domain.usecase.metadata.model.EventType;
+import org.alfresco.hxi_connector.live_ingester.domain.usecase.metadata.model.PropertyDelta;
+import org.alfresco.repo.event.v1.model.ContentInfo;
 import org.alfresco.repo.event.v1.model.DataAttributes;
 import org.alfresco.repo.event.v1.model.NodeResource;
 import org.alfresco.repo.event.v1.model.RepoEvent;
@@ -56,6 +60,9 @@ public final class EventUtils
     public static final String PREDICTION_NODE_TYPE = "hxi:prediction";
     public static final String PREDICTION_APPLIED_ASPECT = "hxi:predictionApplied";
     public static final String PREDICTION_TIME_PROPERTY = "hxi:latestPredictionDateTime";
+    public static final String PREDICTION_VALUE_PROPERTY = "hxi:predictionValue";
+    public static final String PREDICTION_REVIEW_STATUS_PROPERTY = "hxi:reviewStatus";
+    public static final String PREDICTION_CONFIRMED = "CONFIRMED";
 
     public static boolean isEventTypeCreated(RepoEvent<DataAttributes<NodeResource>> event)
     {
@@ -89,12 +96,12 @@ public final class EventUtils
         throw new LiveIngesterRuntimeException("Unsupported Repo event type " + event.getType());
     }
 
-    public static boolean isNotPredictionNodeEvent(RepoEvent<DataAttributes<NodeResource>> event)
+    public static boolean isPredictionNodeEvent(RepoEvent<DataAttributes<NodeResource>> event)
     {
-        return !PREDICTION_NODE_TYPE.equals(event.getData().getResource().getNodeType());
+        return PREDICTION_NODE_TYPE.equals(event.getData().getResource().getNodeType());
     }
 
-    public static boolean isNotPredictionApplyEvent(RepoEvent<DataAttributes<NodeResource>> event)
+    public static boolean isPredictionApplyEvent(RepoEvent<DataAttributes<NodeResource>> event)
     {
         Set<String> aspects = Optional.ofNullable(event.getData().getResource().getAspectNames()).orElse(Collections.emptySet());
         if (aspects.contains(PREDICTION_APPLIED_ASPECT))
@@ -108,10 +115,89 @@ public final class EventUtils
             if (beforeProperties != null && beforeProperties.containsKey(PREDICTION_TIME_PROPERTY))
             {
                 String beforePredictionTime = (String) beforeProperties.get(PREDICTION_TIME_PROPERTY);
-                return Objects.equals(actualPredictionTime, beforePredictionTime);
+                return !Objects.equals(actualPredictionTime, beforePredictionTime);
             }
         }
 
-        return true;
+        return false;
+    }
+
+    /**
+     * We can determine if there is new content that needs processing by looking at the resourceBefore.content and resource.content fields.
+     * <p>
+     * For newly created nodes we have:
+     * <ul>
+     * <li>null -> zero bytes: No content
+     * <li>null -> non-zero bytes: New content
+     * </ul>
+     * For updated nodes we have:
+     * <ul>
+     * <li>null -> zero bytes: No content
+     * <li>null -> non-zero bytes: No change to content
+     * <li>non-zero bytes -> zero bytes: Content deleted
+     * <li>non-zero bytes -> non-zero bytes : Content updated
+     * <li>zero bytes -> non-zero bytes : Content added (no content on node before)
+     * </ul>
+     */
+    public static boolean wasContentChanged(RepoEvent<DataAttributes<NodeResource>> event)
+    {
+        Optional<ContentInfo> latestContentInfo = ofNullable(event.getData().getResource()).map(NodeResource::getContent);
+        // If there's no content info in the current resource then the node cannot contain content.
+        if (latestContentInfo.isEmpty())
+        {
+            return false;
+        }
+        boolean latestContentPresent = !latestContentInfo.get().getSizeInBytes().equals(0L);
+        // If there is content on a new node then we should process it.
+        if (isEventTypeCreated(event))
+        {
+            return latestContentPresent;
+        }
+        else if (isEventTypeUpdated(event))
+        {
+            Optional<ContentInfo> oldContentInfo = ofNullable(event.getData().getResourceBefore()).map(NodeResource::getContent);
+            // We only need to process the content if it was mentioned in the resourceBefore _and_ is non-zero now.
+            return oldContentInfo.isPresent() && latestContentPresent;
+        }
+        // For events other than create or update then we do not need to process the content.
+        return false;
+    }
+
+    public static boolean wasNodePropertyChangedTo(RepoEvent<DataAttributes<NodeResource>> event, String propertyName, Object value)
+    {
+        Map<String, Serializable> oldProperties = event.getData().getResourceBefore().getProperties();
+        Map<String, Serializable> newProperties = event.getData().getResource().getProperties();
+
+        if (oldProperties == null || newProperties == null)
+        {
+            return false;
+        }
+
+        Object oldValue = oldProperties.get(propertyName);
+        Object newValue = newProperties.get(propertyName);
+
+        if (!newValue.equals(value))
+        {
+            return false;
+        }
+
+        return !oldValue.equals(newValue);
+    }
+
+    public static String getNodeParent(RepoEvent<DataAttributes<NodeResource>> event)
+    {
+        return event.getData().getResource()
+                .getPrimaryHierarchy()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new LiveIngesterRuntimeException("Node %s has no parent".formatted(event.getData().getResource())));
+    }
+
+    public static Set<PropertyDelta<?>> getPredictionNodeProperties(RepoEvent<DataAttributes<NodeResource>> event)
+    {
+        String predictedPropertyName = event.getData().getResource().getPrimaryAssocQName();
+        Serializable predictionValue = event.getData().getResource().getProperties().get(PREDICTION_VALUE_PROPERTY);
+
+        return Set.of(PropertyDelta.updated(predictedPropertyName, predictionValue));
     }
 }
