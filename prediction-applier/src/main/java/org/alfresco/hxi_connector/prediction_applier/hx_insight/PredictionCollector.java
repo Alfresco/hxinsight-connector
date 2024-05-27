7f0809e1e8f0422e2a2c93bce18a9bfb9e7a4ce0
Pawel Rainer <pawel.rainer@hyland.com>
2024-05-27 13:37:57 +0200

    ACS-7586: mark processed batches as completed

--- prediction-applier/src/main/java/org/alfresco/hxi_connector/prediction_applier/hx_insight/PredictionCollector.java
+++ prediction-applier/src/main/java/org/alfresco/hxi_connector/prediction_applier/hx_insight/PredictionCollector.java
@@ -38,6 +38,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Predicate;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.ValueBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -64,25 +65,14 @@ public class PredictionCollector extends RouteBuilder
     private static final String PREDICTIONS_PAGE_NO_HEADER = "predictionsPageNo";
     private static final String BATCHES_URL_PATTERN = "%s/prediction-batches?httpMethod=GET&status=APPROVED&page=${headers.%s}";
     private static final String PREDICTIONS_URL_PATTERN = "%s/prediction-batches/${headers.%s}?httpMethod=GET&page=${headers.%s}";
+    private static final String PREDICTIONS_CONFIRMATION_URL_PATTERN = "%s/prediction-batches/${headers.%s}?httpMethod=PUT";
 
     private final InsightPredictionsProperties insightPredictionsProperties;
 
     // @formatter:off
     /**
      * Polls all available prediction batches and sends them (one by one) to the internal buffer for further processing.
-     * <p>
-     * You can imagine that you have the following predictions waiting in hxi: 1. [prediction1, prediction2] 2. [prediction3, prediction4]
-     * <p>
-     * Processor PREDICTION_PROCESSOR after being triggered will:
-     * 1. Fetch the first batch (prediction1, prediction2)
-     *  1.1. Send prediction1 to the internal buffer
-     *  1.2. Send prediction2 to the internal buffer
-     * 2. Fetch the second batch (prediction3, prediction4)
-     *  2.1. Send prediction3 to the internal buffer
-     *  2.2. Send prediction4 to the internal buffer
-     * 3. Exit and wait for the next trigger
-     * <p>
-     * By default, the processor is triggered by the quartz scheduler every 5 minutes.
+     * <a href="https://hyland.atlassian.net/wiki/spaces/HxAI/pages/1387364585/Insight+Enrichment+API+Design">Hx Insight api design</a>
      */
     @Override
     public void configure()
@@ -105,7 +95,9 @@ public class PredictionCollector extends RouteBuilder
 
         String batchesUrl = BATCHES_URL_PATTERN.formatted(insightPredictionsProperties.sourceBaseUrl(), BATCHES_PAGE_NO_HEADER);
         String predictionsUrl = PREDICTIONS_URL_PATTERN.formatted(insightPredictionsProperties.sourceBaseUrl(), BATCH_ID_HEADER, PREDICTIONS_PAGE_NO_HEADER);
+        String predictionsConfirmationUrl = PREDICTIONS_CONFIRMATION_URL_PATTERN.formatted(insightPredictionsProperties.sourceBaseUrl(), BATCH_ID_HEADER);
         SecurityContext securityContext = SecurityContextHolder.getContext();
+
         from(DIRECT_ENDPOINT)
             .routeId(COLLECTOR_ROUTE_ID)
             .process(setProcessingPending(true))
@@ -113,33 +105,33 @@ public class PredictionCollector extends RouteBuilder
                 .process(setProcessingPending(false))
             .end()
             .process(exchange -> AuthSupport.setAuthorizationToken(securityContext, exchange))
-            .setBody(constant("temp-val-for-init"))
             .setHeader(BATCHES_PAGE_NO_HEADER, constant(1))
-            .loopDoWhile(bodyNotEmpty())
-                .log(DEBUG, log, "Calling URL: " + batchesUrl)
                 .toD(batchesUrl)
                 .log(DEBUG, log, "Processing prediction batches: ${body}")
-                .choice().when(bodyNotEmpty())
+                .choice().when(statusCodeNot204())
                     .unmarshal(predictionsBatchDataFormat)
                     .split(body())
+                        .log(TRACE, log, "Processing prediction batch ${body.id} started")
                         .setHeader(BATCH_ID_HEADER, simple("${body.id}"))
-                        // notify HxI -> started batch processing
                         .setHeader(PREDICTIONS_PAGE_NO_HEADER, constant(1))
-                        .loopDoWhile(bodyNotEmpty())
-                            .log(DEBUG, log, "Calling URL: " + predictionsUrl)
+                        .loopDoWhile(statusCodeNot204())
+                            .log(TRACE, log, "Looking for predictions in batch: " + predictionsUrl)
                             .toD(predictionsUrl)
                             .log(DEBUG, log, "Sending predictions to internal buffer: ${body}")
-                            .choice().when(bodyNotEmpty())
+                            .choice().when(statusCodeNot204())
                                 .unmarshal(predictionsDataFormat)
                                 .split(body())
                                     .marshal(predictionDataFormat)
                                     .log(TRACE, log, "Sending prediction to internal buffer: ${body}")
                                     .to(insightPredictionsProperties.bufferEndpoint())
                                 .end()
-                            .end()
+                                .setBody(simple("{\"status\": \"COMPLETE\", \"currentPage\": ${headers.%s}}".formatted(PREDICTIONS_PAGE_NO_HEADER)))
+                                .toD(predictionsConfirmationUrl)
+                                .log(TRACE, log, "Processing prediction batch ${headers.%s} page ${headers.%s} completed".formatted(BATCH_ID_HEADER, PREDICTIONS_PAGE_NO_HEADER))
+                                .end()
                             .setHeader(PREDICTIONS_PAGE_NO_HEADER).spel("#{request.headers['%s'] + 1}".formatted(PREDICTIONS_PAGE_NO_HEADER))
                         .end()
-                        // notify HxI -> finished batch processing
+                        .log(TRACE, log, "Processing prediction batch ${headers.%s} finished".formatted(BATCH_ID_HEADER))
                     .end()
                 .end()
                 .setHeader(BATCHES_PAGE_NO_HEADER).spel("#{request.headers['%s'] + 1}".formatted(BATCHES_PAGE_NO_HEADER))
@@ -149,9 +141,8 @@ public class PredictionCollector extends RouteBuilder
     }
     // @formatter:on
 
-    private Predicate bodyNotEmpty()
-    {
-        return and(body().isNotNull(), bodyAs(Collection.class).method("isEmpty").isEqualTo(false));
+    private ValueBuilder statusCodeNot204() {
+        return simple("${header.CamelHttpResponseCode} != 204");
     }
 
     private boolean isProcessingPending(Exchange exchange)
