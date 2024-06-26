d8d1f05734ca0aa454c13d8e2de28647c842bb58
Krystian Dabrowski <krystian.dabrowski@hyland.com>
2024-06-26 20:38:05 +0200

    ACS-8278: rename package prediction_applier to hxi_extension

--- prediction-applier/src/main/java/org/alfresco/hxi_connector/prediction_applier/PredictionApplierApplication.java
+++ prediction-applier/src/main/java/org/alfresco/hxi_connector/prediction_applier/PredictionApplierApplication.java
@@ -29,10 +29,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 @SuppressWarnings("PMD.UseUtilityClass")
