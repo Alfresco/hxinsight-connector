34d930ae10671d7d75c5cd5e124684ff90beeb6b
Pawel Rainer <pawel.rainer@hyland.com>
2024-07-12 12:51:02 +0200

    ACS-8130: check users permissions

--- hxinsight-extension/src/main/java/org/alfresco/hxi_connector/hxi_extension/service/QuestionPermissionService.java
+++ hxinsight-extension/src/main/java/org/alfresco/hxi_connector/hxi_extension/service/QuestionPermissionService.java
@@ -24,18 +24,30 @@
  * #L%
  */
 
-package org.alfresco.hxi_connector.hxi_extension.service.model;
+package org.alfresco.hxi_connector.hxi_extension.service;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import lombok.Getter;
+import static org.alfresco.hxi_connector.hxi_extension.rest.api.util.NodesUtils.validateOrLookupNode;
+
 import lombok.RequiredArgsConstructor;
 
-@Getter
+import org.alfresco.hxi_connector.hxi_extension.service.model.Question;
+import org.alfresco.rest.api.Nodes;
+import org.alfresco.service.cmr.security.AccessStatus;
+import org.alfresco.service.cmr.security.PermissionService;
+
 @RequiredArgsConstructor
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class Question
+public class QuestionPermissionService
 {
-    private final String question;
-    private final String agentId;
-    private final RestrictionQuery restrictionQuery;
+    private final Nodes nodes;
+    private final PermissionService permissionService;
+
+    public boolean hasPermissionToAskAboutDocuments(Question question)
+    {
+        return question.getRestrictionQuery()
+                .nodesIds()
+                .stream()
+                .map(nodeId -> validateOrLookupNode(nodes, nodeId))
+                .map(permissionService::hasReadPermission)
+                .allMatch(accessStatus -> accessStatus.equals(AccessStatus.ALLOWED));
+    }
 }
