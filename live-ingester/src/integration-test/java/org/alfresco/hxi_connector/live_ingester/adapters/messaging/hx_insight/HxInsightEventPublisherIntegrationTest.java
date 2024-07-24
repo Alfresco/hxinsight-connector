6f00b33bf095d7fb249066c2ef41b424025f7c07
mpichura <maciej.pichura@hyland.com>
2024-07-24 16:48:20 +0200

    ACS-8464: Fixing tests.

--- live-ingester/src/integration-test/java/org/alfresco/hxi_connector/live_ingester/adapters/messaging/hx_insight/HxInsightEventPublisherIntegrationTest.java
+++ live-ingester/src/integration-test/java/org/alfresco/hxi_connector/live_ingester/adapters/messaging/hx_insight/HxInsightEventPublisherIntegrationTest.java
@@ -81,9 +81,11 @@ import org.alfresco.hxi_connector.common.adapters.auth.config.properties.AuthPro
 import org.alfresco.hxi_connector.common.adapters.auth.util.AuthUtils;
 import org.alfresco.hxi_connector.common.adapters.messaging.repository.ApplicationInfoProvider;
 import org.alfresco.hxi_connector.common.adapters.messaging.repository.api.DiscoveryApiClient;
+import org.alfresco.hxi_connector.common.config.properties.Application;
 import org.alfresco.hxi_connector.common.exception.EndpointClientErrorException;
 import org.alfresco.hxi_connector.common.exception.EndpointServerErrorException;
 import org.alfresco.hxi_connector.common.test.docker.util.DockerContainers;
+import org.alfresco.hxi_connector.common.test.docker.util.DockerTags;
 import org.alfresco.hxi_connector.live_ingester.adapters.auth.LiveIngesterAuthClient;
 import org.alfresco.hxi_connector.live_ingester.adapters.config.IntegrationProperties;
 import org.alfresco.hxi_connector.live_ingester.domain.ports.ingestion_engine.IngestionEngineEventPublisher;
@@ -215,6 +217,12 @@ class HxInsightEventPublisherIntegrationTest
         public AuthService authService()
         {
             return new AuthService(authorizationProperties(), defaultAccessTokenProvider());
+        }
+
+        @Bean
+        public Application application()
+        {
+            return new Application("alfresco-dummy-source-id-0a63de491876", DockerTags.getHxiConnectorTag());
         }
     }
 }
