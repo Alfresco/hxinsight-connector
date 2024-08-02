4efa459bd5840a0e4d92a8ea17de7f51df1eccf4
Krystian Dąbrowski <krystian.dabrowski@hyland.com>
2024-08-02 17:48:22 +0200

    ACS-8505: Revert docker-compose run for CreateNodeE2eTest - merge master

--- e2e-test/src/test/java/org/alfresco/hxi_connector/e2e_test/CreateNodeE2eTest.java
+++ e2e-test/src/test/java/org/alfresco/hxi_connector/e2e_test/CreateNodeE2eTest.java
@@ -27,16 +27,21 @@ package org.alfresco.hxi_connector.e2e_test;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.exactly;
+import static com.github.tomakehurst.wiremock.client.WireMock.findAll;
 import static com.github.tomakehurst.wiremock.client.WireMock.matching;
 import static com.github.tomakehurst.wiremock.client.WireMock.moreThanOrExactly;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static org.alfresco.hxi_connector.common.constant.HttpHeaders.USER_AGENT;
 import static org.alfresco.hxi_connector.common.test.docker.repository.RepositoryType.ENTERPRISE;
 import static org.alfresco.hxi_connector.common.test.docker.util.DockerContainers.getAppInfoRegex;
 import static org.alfresco.hxi_connector.common.test.docker.util.DockerContainers.getRepoJavaOptsWithTransforms;
+import static org.alfresco.hxi_connector.e2e_test.util.TestJsonUtils.asSet;
 import static org.alfresco.hxi_connector.e2e_test.util.client.RepositoryClient.ADMIN_USER;
 
 import java.io.ByteArrayInputStream;
@@ -45,8 +50,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.verification.LoggedRequest;
 import lombok.Cleanup;
 import lombok.SneakyThrows;
 import org.apache.commons.collections4.CollectionUtils;
@@ -80,11 +90,14 @@ import org.alfresco.hxi_connector.e2e_test.util.client.model.S3Object;
 @SuppressWarnings({"PMD.FieldNamingConventions", "PMD.TestClassWithoutTestCases"})
 public class CreateNodeE2eTest
 {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     protected static final String BUCKET_NAME = "test-hxinsight-bucket";
     private static final int MAX_ATTEMPTS = 5;
-    private static final int INITIAL_DELAY_MS = 500;
+    private static final int INITIAL_DELAY_MS = 700;
     private static final String PARENT_ID = "-my-";
     private static final String DUMMY_CONTENT = "Dummy's file dummy content";
+    private static final String ALLOW_ACCESS_PROPERTY = "ALLOW_ACCESS";
+    private static final String DENY_ACCESS_PROPERTY = "DENY_ACCESS";
 
     protected RepositoryClient repositoryClient;
     protected AwsS3Client awsS3Client;
@@ -178,6 +191,41 @@ public class CreateNodeE2eTest
             WireMock.verify(moreThanOrExactly(2), postRequestedFor(urlEqualTo("/ingestion-events"))
                     .withRequestBody(containing(createdNode.id()))
                     .withHeader(USER_AGENT, matching(getAppInfoRegex())));
+        }, MAX_ATTEMPTS, INITIAL_DELAY_MS);
+    }
+
+    @Test
+    @SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert"})
+    final void testCreateNodeWithDefaultPermissions()
+    {
+        // when
+        Node createdNode = repositoryClient.createNodeWithContent(
+                PARENT_ID,
+                "test file",
+                new ByteArrayInputStream("test file content".getBytes()),
+                "text/plain");
+
+        // then
+        RetryUtils.retryWithBackoff(() -> {
+            List<LoggedRequest> requests = findAll(postRequestedFor(urlEqualTo("/ingestion-events")));
+
+            assertFalse(requests.isEmpty());
+
+            Optional<LoggedRequest> createNodeEvent = requests.stream()
+                    .filter(request -> request.getBodyAsString().contains(createdNode.id()))
+                    .findFirst();
+
+            assertTrue(createNodeEvent.isPresent());
+
+            JsonNode properties = objectMapper.readTree(createNodeEvent.get().getBodyAsString())
+                    .get(0)
+                    .get("properties");
+
+            assertTrue(properties.has(ALLOW_ACCESS_PROPERTY));
+            assertEquals(Set.of("GROUP_EVERYONE"), asSet(properties.get(ALLOW_ACCESS_PROPERTY).get("value")));
+
+            assertTrue(properties.has(DENY_ACCESS_PROPERTY));
+            assertEquals(Set.of(), asSet(properties.get(DENY_ACCESS_PROPERTY).get("value")));
         }, MAX_ATTEMPTS, INITIAL_DELAY_MS);
     }
 
