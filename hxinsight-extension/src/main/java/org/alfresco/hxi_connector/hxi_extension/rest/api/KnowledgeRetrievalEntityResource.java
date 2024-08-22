8e442bac6e6b44db820f65f2fe3fee5c0e2635d9
Pawel Rainer <pawel.rainer@hyland.com>
2024-08-22 13:21:05 +0200

    ACS-8628: add knowledge retrieval url endpoint

---
+++ hxinsight-extension/src/main/java/org/alfresco/hxi_connector/hxi_extension/rest/api/KnowledgeRetrievalEntityResource.java
@@ -0,0 +1,47 @@
+/*-
+ * #%L
+ * Alfresco HX Insight Connector
+ * %%
+ * Copyright (C) 2023 - 2024 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.hxi_connector.hxi_extension.rest.api;
+
+import lombok.RequiredArgsConstructor;
+import org.alfresco.rest.framework.resource.EntityResource;
+import org.alfresco.rest.framework.resource.actions.interfaces.EntityResourceAction;
+import org.alfresco.rest.framework.resource.parameters.CollectionWithPagingInfo;
+import org.alfresco.rest.framework.resource.parameters.Parameters;
+
+import java.util.Set;
+
+@RequiredArgsConstructor
+@EntityResource(name = "knowledge-retrieval", title = "Hyland Knowledge Retrieval Url")
+public class KnowledgeRetrievalEntityResource implements EntityResourceAction.Read<String>
+{
+    private final String knowledgeRetrievalUrl;
+
+    @Override
+    public CollectionWithPagingInfo<String> readAll(Parameters params)
+    {
+        return CollectionWithPagingInfo.asPaged(params.getPaging(), Set.of(knowledgeRetrievalUrl));
+    }
+}
