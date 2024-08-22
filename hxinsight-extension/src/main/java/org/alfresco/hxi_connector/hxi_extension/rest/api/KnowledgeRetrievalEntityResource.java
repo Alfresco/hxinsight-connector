8edc7695fc4df3559f27cca08e221ee204d036e8
Pawel Rainer <pawel.rainer@hyland.com>
2024-08-22 13:25:22 +0200

    ACS-8628: add knowledge retrieval url endpoint

--- hxinsight-extension/src/main/java/org/alfresco/hxi_connector/hxi_extension/rest/api/KnowledgeRetrievalEntityResource.java
+++ hxinsight-extension/src/main/java/org/alfresco/hxi_connector/hxi_extension/rest/api/KnowledgeRetrievalEntityResource.java
@@ -28,11 +28,11 @@ package org.alfresco.hxi_connector.hxi_extension.rest.api;
 import java.util.Set;
 
 import lombok.RequiredArgsConstructor;
+
 import org.alfresco.rest.framework.resource.EntityResource;
 import org.alfresco.rest.framework.resource.actions.interfaces.EntityResourceAction;
 import org.alfresco.rest.framework.resource.parameters.CollectionWithPagingInfo;
 import org.alfresco.rest.framework.resource.parameters.Parameters;
-
 
 @RequiredArgsConstructor
 @EntityResource(name = "knowledge-retrieval", title = "Hyland Knowledge Retrieval Url")
