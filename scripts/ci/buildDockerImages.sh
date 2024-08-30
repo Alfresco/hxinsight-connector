84849741d2784357235d79ae3e46bb3ae5562e62
Krystian Dąbrowski <krystian.dabrowski@hyland.com>
2024-08-30 16:49:24 +0200

    NO_JIRA: verify Docker build on CI

--- scripts/ci/buildDockerImages.sh
+++ scripts/ci/buildDockerImages.sh
@@ -73,7 +73,7 @@ do
       DOCKER_TAG=${PROJECT_VERSION}
     fi
     unset JAVA_VERSION
-    docker build --label "GIT_COMMIT=${COMMIT_MESSAGE}" --label "GIT_BRANCH=${GITHUB_REF_NAME}" --tag "${DOCKER_REGISTRY}/${DOCKER_IMAGE_REPOSITORY}:${DOCKER_TAG}" .
+    docker build --label "GIT_COMMIT=${COMMIT_MESSAGE}" --label "GIT_BRANCH=${GITHUB_REF_NAME}" --tag "${DOCKER_REGISTRY}/${DOCKER_IMAGE_REPOSITORY}:${DOCKER_TAG}" --build-arg="JAVA_VERSION=11" .
   fi
 
   cd -
