11258530f0a1296925b2f0a687091474cc692358
Pawel Rainer <pawel.rainer@hyland.com>
2024-09-13 12:41:17 +0200

    ACS-8768: add tests

---
+++ e2e-test/src/test/java/org/alfresco/hxi_connector/e2e_test/LiveIngesterWithUnreachableACSE2eTest.java
@@ -0,0 +1,134 @@
+/*-
+ * #%L
+ * Alfresco HX Insight Connector
+ * %%
+ * Copyright (C) 2023 - 2024 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.hxi_connector.e2e_test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.alfresco.hxi_connector.common.test.docker.repository.RepositoryType.ENTERPRISE;
+import static org.alfresco.hxi_connector.common.test.docker.util.DockerContainers.getRepoJavaOptsWithTransforms;
+import static org.alfresco.hxi_connector.e2e_test.util.client.RepositoryClient.ADMIN_USER;
+
+import java.io.ByteArrayInputStream;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.wiremock.integrations.testcontainers.WireMockContainer;
+
+import org.alfresco.hxi_connector.common.test.docker.repository.AlfrescoRepositoryContainer;
+import org.alfresco.hxi_connector.common.test.docker.util.DockerContainers;
+import org.alfresco.hxi_connector.e2e_test.util.client.RepositoryClient;
+
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SuppressWarnings({"PMD.FieldNamingConventions", "PMD.TestClassWithoutTestCases"})
+public class LiveIngesterWithUnreachableACSE2eTest
+{
+    private static final String PARENT_ID = "-my-";
+
+    private static final Network network = Network.newNetwork();
+    @Container
+    private static final PostgreSQLContainer<?> postgres = DockerContainers.createPostgresContainerWithin(network);
+    @Container
+    private static final GenericContainer<?> activemq = DockerContainers.createActiveMqContainerWithin(network);
+    @Container
+    private static final GenericContainer<?> sfs = DockerContainers.createSfsContainerWithin(network);
+    @Container
+    private static final GenericContainer<?> transformCore = DockerContainers.createTransformCoreAioContainerWithin(network)
+            .dependsOn(activemq);
+    @Container
+    private static final GenericContainer<?> transformRouter = DockerContainers.createTransformRouterContainerWithin(network)
+            .dependsOn(activemq, transformCore);
+    @Container
+    private static final WireMockContainer hxInsightMock = DockerContainers.createWireMockContainerWithin(network)
+            .withFileSystemBind("src/test/resources/wiremock/hxinsight", "/home/wiremock", BindMode.READ_ONLY);
+    @Container
+    private static final LocalStackContainer awsMock = DockerContainers.createLocalStackContainerWithin(network);
+    @Container
+    private static final AlfrescoRepositoryContainer repository = createRepositoryContainer()
+            .dependsOn(postgres, activemq, transformRouter, sfs);
+    @Container
+    private static final GenericContainer<?> liveIngester = createLiveIngesterContainer()
+            .withEnv("ALFRESCO_REPOSITORY_BASE_URL", "http://localhost:1938/alfresco") // <- random unreachable port
+            .dependsOn(activemq, hxInsightMock, awsMock, repository);
+
+    private RepositoryClient repositoryClient;
+
+    @BeforeAll
+    public void beforeAll()
+    {
+        repositoryClient = new RepositoryClient(repository.getBaseUrl(), ADMIN_USER);
+        WireMock.configureFor(hxInsightMock.getHost(), hxInsightMock.getPort());
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        WireMock.reset();
+    }
+
+    @Test
+    @SneakyThrows
+    @SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert"})
+    final void shouldNotProcessAnyEventsIfRepoIsUnreachable()
+    {
+        // given
+        repositoryClient.createNodeWithContent(
+                PARENT_ID,
+                "test file",
+                new ByteArrayInputStream("test file content".getBytes()),
+                "text/plain");
+
+        // when
+        Thread.sleep(2000);
+
+        // then
+        assertEquals(0, WireMock.findAll(RequestPatternBuilder.allRequests()).size());
+    }
+
+    private static AlfrescoRepositoryContainer createRepositoryContainer()
+    {
+        return DockerContainers.createExtendedRepositoryContainerWithin(network, ENTERPRISE)
+                .withJavaOpts(getRepoJavaOptsWithTransforms(postgres, activemq));
+    }
+
+    private static GenericContainer<?> createLiveIngesterContainer()
+    {
+        return DockerContainers.createLiveIngesterContainerForWireMock(hxInsightMock, repository, network)
+                .withEnv("ALFRESCO_REPOSITORY_HEALTH_PROBE_INTERVAL_SECONDS", "1");
+    }
+}
