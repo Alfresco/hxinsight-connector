d0b4c69b10f4c0708d15d00d19802998d1ef2ba3
Pawel Rainer <pawel.rainer@hyland.com>
2025-01-16 11:05:14 +0100

    fix: fix tests

--- prediction-applier/src/integration-test/java/org/alfresco/hxi_connector/prediction_applier/util/ContainerSupport.java
+++ prediction-applier/src/integration-test/java/org/alfresco/hxi_connector/prediction_applier/util/ContainerSupport.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco HX Insight Connector
  * %%
- * Copyright (C) 2023 - 2024 Alfresco Software Limited
+ * Copyright (C) 2023 - 2025 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * If the software was purchased under a paid Alfresco license, the terms of
@@ -57,7 +57,7 @@ public class ContainerSupport
 {
     public static final String HXI_PREDICTION_BATCHES_ENDPOINT = "/prediction-batches";
     public static final String REPOSITORY_PREDICTION_ENDPOINT = "/api/-default-/private/hxi/versions/1/nodes/%s/predictions";
-    public static final String USER_AGENT_REGEX = "ACS HXI Connector\\/" + DockerTags.getHxiConnectorTag() + " ACS\\/" + DockerTags.getRepositoryTag() + " .*";
+    public static final String USER_AGENT_REGEX = "ACS HXI Connector\\/" + DockerTags.getHxiConnectorTag() + " ACS\\/" + DockerTags.getRepositoryTag().replaceAll("\\.\\d+$", "") + " .*";
     public static final String DISCOVERY_ENDPOINT = "/api/discovery";
     private static ContainerSupport instance;
     private final Session session;
