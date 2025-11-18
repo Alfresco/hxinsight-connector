f9b4a583cb277ba34a7bdf4ccb31f50a86e9205b
angshumang65 <Angshuman.Ghosh@hyland.com>
2025-11-18 18:44:31 +0530

    Added user-mapping-sync-processor

---
+++ nucleus-sync/src/main/java/org/alfresco/hxi_connector/nucleus_sync/services/processors/UserMappingSyncProcessor.java
@@ -0,0 +1,152 @@
+/*-
+ * #%L
+ * Alfresco HX Insight Connector
+ * %%
+ * Copyright (C) 2023 - 2025 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.hxi_connector.nucleus_sync.services.processors;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import org.alfresco.hxi_connector.nucleus_sync.client.NucleusClient;
+import org.alfresco.hxi_connector.nucleus_sync.dto.AlfrescoUser;
+import org.alfresco.hxi_connector.nucleus_sync.dto.IamUser;
+import org.alfresco.hxi_connector.nucleus_sync.dto.NucleusUserMappingInput;
+import org.alfresco.hxi_connector.nucleus_sync.dto.NucleusUserMappingOutput;
+import org.alfresco.hxi_connector.nucleus_sync.model.UserMapping;
+
+@Service
+@RequiredArgsConstructor
+public class UserMappingSyncProcessor
+{
+    private final NucleusClient nucleusClient;
+    private static final Logger logger = LoggerFactory.getLogger(UserMappingSyncProcessor.class);
+
+    /**
+     * Sync alfresco and nucleus user mappings with nucleus and local db
+     *
+     * @param alfrescoUsers
+     *            list of alfresco users
+     * @param nucleusIamUsers
+     *            list of IAM users of nucleus
+     * @param currentUserMappings
+     *            current nucleus user mappings
+     * @return list of updated user mappings
+     */
+    public List<UserMapping> syncUserMappings(
+            List<AlfrescoUser> alfrescoUsers,
+            List<IamUser> nucleusIamUsers,
+            List<NucleusUserMappingOutput> currentUserMappings)
+    {
+
+        // AlfrescoUsers must have email to map
+        Map<String, AlfrescoUser> alfrescoUserByEmail = alfrescoUsers.stream()
+                .filter(u -> u.getEmail() != null && !u.getEmail().isEmpty())
+                .collect(Collectors.toMap(AlfrescoUser::getEmail, Function.identity()));
+
+        Map<String, IamUser> nucleusIamUserByEmail = nucleusIamUsers.stream()
+                .collect(Collectors.toMap(IamUser::getEmail, Function.identity()));
+
+        Map<String, NucleusUserMappingOutput> nucleusMappingByAlfrescoId = currentUserMappings.stream()
+                .collect(
+                        Collectors.toMap(
+                                NucleusUserMappingOutput::getExternalUserId,
+                                Function.identity()));
+
+        List<String> nucleusMappingsToDelete = new ArrayList<>();
+        List<NucleusUserMappingInput> nucleusMappingsToCreate = new ArrayList<>();
+        List<UserMapping> cachedMappings = new ArrayList<>();
+        Set<String> validAlfrescoIds = new HashSet<>();
+
+        Set<String> allEmails = new HashSet<>();
+        allEmails.addAll(alfrescoUserByEmail.keySet());
+        allEmails.addAll(nucleusIamUserByEmail.keySet());
+
+        for (String email : allEmails)
+        {
+            AlfrescoUser alfrescoUser = alfrescoUserByEmail.get(email);
+            IamUser nucleusIamUser = nucleusIamUserByEmail.get(email);
+
+            boolean isAlfrescoUser = alfrescoUser != null;
+            boolean isNucleusUser = nucleusIamUser != null;
+
+            if (isAlfrescoUser && isNucleusUser)
+            {
+                String alfrescoUserId = alfrescoUser.getId();
+                String nucleusUserId = nucleusIamUser.getUserId();
+
+                validAlfrescoIds.add(alfrescoUserId);
+                cachedMappings.add(
+                        new UserMapping(alfrescoUser.getEmail(), alfrescoUserId, nucleusUserId));
+
+                if (!nucleusMappingByAlfrescoId.containsKey(alfrescoUserId))
+                {
+                    nucleusMappingsToCreate.add(
+                            new NucleusUserMappingInput(nucleusUserId, alfrescoUserId));
+                }
+            }
+        }
+
+        for (NucleusUserMappingOutput nucleusMapping : currentUserMappings)
+        {
+            String alfrescoUserId = nucleusMapping.getExternalUserId();
+            if (!validAlfrescoIds.contains(alfrescoUserId))
+            {
+                nucleusMappingsToDelete.add(alfrescoUserId);
+            }
+        }
+
+        executeUserBatchOperations(nucleusMappingsToDelete, nucleusMappingsToCreate);
+
+        logger.debug("Final user mappings count: {}", cachedMappings.size());
+        return cachedMappings;
+    }
+
+    private void executeUserBatchOperations(
+            List<String> nucleusMappingsToDelete,
+            List<NucleusUserMappingInput> nucleusMappingsToCreate)
+    {
+
+        for (String alfrescoUserId : nucleusMappingsToDelete)
+        {
+            nucleusClient.deleteUserMapping(alfrescoUserId);
+        }
+        logger.debug("Deleted {} user mapping in Nucleus.", nucleusMappingsToDelete.size());
+
+        if (!nucleusMappingsToCreate.isEmpty())
+        {
+            nucleusClient.createUserMappings(nucleusMappingsToCreate);
+        }
+        logger.debug("Created {} user mappings in nucleus", nucleusMappingsToCreate.size());
+    }
+}
