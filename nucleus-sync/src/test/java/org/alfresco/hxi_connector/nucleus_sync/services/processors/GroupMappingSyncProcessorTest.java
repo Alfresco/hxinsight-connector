96133ae5abbdffd7984e9dd75b2deac131b2582a
angshumang65 <Angshuman.Ghosh@hyland.com>
2025-11-24 14:07:16 +0530

    Added Unit test for GroupMappingSyncProcessor

---
+++ nucleus-sync/src/test/java/org/alfresco/hxi_connector/nucleus_sync/services/processors/GroupMappingSyncProcessorTest.java
@@ -0,0 +1,147 @@
+/*-
+ * #%L
+ * Alfresco HX Insight Connector
+ * %%
+ * Copyright (C) 2023 - 2025 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.hxi_connector.nucleus_sync.services.processors;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.alfresco.hxi_connector.nucleus_sync.client.NucleusClient;
+import org.alfresco.hxi_connector.nucleus_sync.dto.AlfrescoGroup;
+import org.alfresco.hxi_connector.nucleus_sync.dto.NucleusGroupOutput;
+import org.alfresco.hxi_connector.nucleus_sync.model.GroupMapping;
+
+@ExtendWith(MockitoExtension.class)
+public class GroupMappingSyncProcessorTest
+{
+    @Mock
+    private NucleusClient nucleusClient;
+
+    @InjectMocks
+    private GroupMappingSyncProcessor processor;
+
+    @Test
+    void shouldIgnoreGroupsWithNoUserMemberships()
+    {
+        // Given
+        List<AlfrescoGroup> alfrescoGroups = List.of(
+                new AlfrescoGroup("GROUP_ADMINISTRATORS", "Administrators"),
+                new AlfrescoGroup("GROUP_DEVELOPERS", "Developers"),
+                new AlfrescoGroup("GROUP_EMPTY", "Empty Group"));
+        List<NucleusGroupOutput> nucleusGroups = new ArrayList<>();
+        Map<String, List<String>> userGroupMemberships = new HashMap<>();
+        userGroupMemberships.put("user1", List.of("GROUP_ADMINISTRATORS"));
+        userGroupMemberships.put("user2", List.of("GROUP_DEVELOPERS"));
+
+        // When
+        List<GroupMapping> result = processor.syncGroupMappings(alfrescoGroups, nucleusGroups, userGroupMemberships);
+
+        // Then
+        assertThat(result)
+                .hasSize(2)
+                .extracting(GroupMapping::getAlfrescoGroupId)
+                .containsExactlyInAnyOrder("GROUP_ADMINISTRATORS", "GROUP_DEVELOPERS");
+        verify(nucleusClient).createGroups(argThat(groups -> groups.size() == 2));
+    }
+
+    @Test
+    void shouldDeleteStaleGroupsWhenRemovedFromNucleus()
+    {
+        // Given
+        List<AlfrescoGroup> alfrescoGroups = List.of(
+                new AlfrescoGroup("GROUP_ADMINISTRATORS", "Administrators"));
+        List<NucleusGroupOutput> nucleusGroups = List.of(
+                new NucleusGroupOutput("GROUP_OLD"));
+        Map<String, List<String>> userGroupMemberships = new HashMap<>();
+        userGroupMemberships.put("user1", List.of("GROUP_ADMINISTRATORS"));
+
+        // When
+        List<GroupMapping> result = processor.syncGroupMappings(alfrescoGroups, nucleusGroups, userGroupMemberships);
+
+        // Then
+        assertThat(result)
+                .hasSize(1)
+                .extracting(GroupMapping::getAlfrescoGroupId)
+                .containsExactly("GROUP_ADMINISTRATORS");
+        verify(nucleusClient).deleteGroup("GROUP_OLD");
+        verify(nucleusClient).createGroups(argThat(groups -> groups.size() == 1));
+    }
+
+    @Test
+    void shouldHandleNullGroupListsInUserMembershipsCache()
+    {
+        // Given
+        List<AlfrescoGroup> alfrescoGroups = List.of(
+                new AlfrescoGroup("GROUP_ADMINISTRATORS", "Administrators"));
+        List<NucleusGroupOutput> nucleusGroups = new ArrayList<>();
+        Map<String, List<String>> userGroupMemberships = new HashMap<>();
+        userGroupMemberships.put("user1", null);
+        userGroupMemberships.put("user2", List.of("GROUP_ADMINISTRATORS"));
+
+        // When
+        List<GroupMapping> result = processor.syncGroupMappings(alfrescoGroups, nucleusGroups, userGroupMemberships);
+
+        // Then
+        assertThat(result)
+                .hasSize(1)
+                .extracting(GroupMapping::getAlfrescoGroupId)
+                .containsExactly("GROUP_ADMINISTRATORS");
+        verify(nucleusClient).createGroups(argThat(groups -> groups.size() == 1));
+    }
+
+    @Test
+    void shouldKeepExistingMappingsUnchanged()
+    {
+        // Given
+        List<AlfrescoGroup> alfrescoGroups = List.of(
+                new AlfrescoGroup("GROUP_ADMINISTRATORS", "Administrators"));
+        List<NucleusGroupOutput> nucleusGroups = List.of(
+                new NucleusGroupOutput("GROUP_ADMINISTRATORS"));
+        Map<String, List<String>> userGroupMemberships = new HashMap<>();
+        userGroupMemberships.put("user1", List.of("GROUP_ADMINISTRATORS"));
+
+        // When
+        List<GroupMapping> result = processor.syncGroupMappings(alfrescoGroups, nucleusGroups, userGroupMemberships);
+
+        // Then
+        assertThat(result)
+                .hasSize(1)
+                .extracting(GroupMapping::getAlfrescoGroupId)
+                .containsExactly("GROUP_ADMINISTRATORS");
+        verify(nucleusClient, never()).deleteGroup(any());
+        verify(nucleusClient, never()).createGroups(anyList());
+    }
+}
