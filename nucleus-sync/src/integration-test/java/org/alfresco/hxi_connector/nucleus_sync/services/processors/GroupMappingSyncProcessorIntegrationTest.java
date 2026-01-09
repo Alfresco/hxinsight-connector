cb061529064798c1054968b03afa9e2fe19e2bd9
angshumang65 <Angshuman.Ghosh@hyland.com>
2026-01-09 17:02:22 +0530

    Added GroupMappingProcessor

---
+++ nucleus-sync/src/integration-test/java/org/alfresco/hxi_connector/nucleus_sync/services/processors/GroupMappingSyncProcessorIntegrationTest.java
@@ -0,0 +1,110 @@
+/*-
+ * #%L
+ * Alfresco HX Insight Connector
+ * %%
+ * Copyright (C) 2023 - 2026 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.hxi_connector.nucleus_sync.services.processors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.anyList;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import org.alfresco.hxi_connector.nucleus_sync.client.NucleusClient;
+import org.alfresco.hxi_connector.nucleus_sync.dto.AlfrescoGroup;
+import org.alfresco.hxi_connector.nucleus_sync.dto.NucleusGroupInput;
+import org.alfresco.hxi_connector.nucleus_sync.dto.NucleusGroupOutput;
+import org.alfresco.hxi_connector.nucleus_sync.model.GroupMapping;
+
+@SpringBootTest(classes = GroupMappingSyncProcessor.class)
+public class GroupMappingSyncProcessorIntegrationTest
+{
+    @Autowired
+    private GroupMappingSyncProcessor processor;
+
+    @MockitoBean
+    private NucleusClient nucleusClient;
+
+    @Test
+    void shouldSyncGroupMappingsWithCreatesDeletesAndUnchangedGroups()
+    {
+        // Given - A scenario with creates, deletes, and unchanged mappings
+        List<AlfrescoGroup> alfrescoGroups = List.of(
+                // new mapping - create
+                new AlfrescoGroup("GROUP_ENGINEERING", "Engineering Team"),
+                // new mapping - create
+                new AlfrescoGroup("GROUP_PRODUCT", "Product Team"),
+                // existing mapping - keep
+                new AlfrescoGroup("GROUP_HR", "Human Resources"),
+                // No user memberships, ignore
+                new AlfrescoGroup("GROUP_EMPTY", "Empty Group"));
+
+        List<NucleusGroupOutput> nucleusGroups = List.of(
+                // Existing mapping, keep
+                new NucleusGroupOutput("GROUP_HR"),
+                // Delete - group removed from alfresco
+                new NucleusGroupOutput("GROUP_SALES"));
+
+        Map<String, List<String>> userGroupMemberships = new HashMap<>();
+        // Users in new groups to create
+        userGroupMemberships.put("jdoe", List.of("GROUP_ENGINEERING", "GROUP_PRODUCT"));
+        userGroupMemberships.put("bsmith", List.of("GROUP_ENGINEERING"));
+        userGroupMemberships.put("mjohnson", List.of("GROUP_PRODUCT", "GROUP_HR"));
+        userGroupMemberships.put("awilliams", List.of("GROUP_DESIGN"));
+        // User with null groups
+        userGroupMemberships.put("tgarcia", null);
+
+        // When
+        List<GroupMapping> result = processor.syncGroupMappings(alfrescoGroups, nucleusGroups, userGroupMemberships);
+
+        // Then - Verify all deletions happened
+        verify(nucleusClient).deleteGroup("GROUP_SALES");
+
+        // Then - Verify creations with correct payload (3 new groups)
+        verify(nucleusClient).createGroups(argThat(groups -> groups.size() == 2 &&
+                groups.contains(new NucleusGroupInput("GROUP_ENGINEERING")) &&
+                groups.contains(new NucleusGroupInput("GROUP_PRODUCT"))));
+
+        // Then - Verify returned mappings are correct (5 total: 3 new + 2 existing)
+        assertThat(result)
+                .containsExactlyInAnyOrder(
+                        new GroupMapping("GROUP_ENGINEERING", "Engineering Team"),
+                        new GroupMapping("GROUP_PRODUCT", "Product Team"),
+                        new GroupMapping("GROUP_HR", "Human Resources"));
+
+        // Then - Verify exactly 2 interactions (1 deletes, 1 create batch)
+        verify(nucleusClient, times(1)).deleteGroup(anyString());
+        verify(nucleusClient, times(1)).createGroups(anyList());
+    }
+}
---
+++ nucleus-sync/src/main/java/org/alfresco/hxi_connector/nucleus_sync/services/processors/GroupMappingSyncProcessor.java
@@ -0,0 +1,166 @@
+/*-
+ * #%L
+ * Alfresco HX Insight Connector
+ * %%
+ * Copyright (C) 2023 - 2026 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.hxi_connector.nucleus_sync.services.processors;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import org.alfresco.hxi_connector.nucleus_sync.client.NucleusClient;
+import org.alfresco.hxi_connector.nucleus_sync.dto.AlfrescoGroup;
+import org.alfresco.hxi_connector.nucleus_sync.dto.NucleusGroupInput;
+import org.alfresco.hxi_connector.nucleus_sync.dto.NucleusGroupOutput;
+import org.alfresco.hxi_connector.nucleus_sync.model.GroupMapping;
+
+@Service
+@RequiredArgsConstructor
+public class GroupMappingSyncProcessor
+{
+    private final NucleusClient nucleusClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(GroupMappingSyncProcessor.class);
+
+    /**
+     * Performs group sync operation with nucleus.
+     *
+     * Only those groups are synced whose users have been synced. Groups with no users or groups with all users who could not be synced are not synced with nucleus.
+     *
+     * @param alfrescoGroups
+     *            list of all alfresco groups
+     * @param currentNucleusGroups
+     *            list of groups from nucleus
+     * @param userGroupMembershipsCache
+     *            cache of user and their groups
+     * @return list of updated group mappings
+     */
+    public List<GroupMapping> syncGroupMappings(
+            List<AlfrescoGroup> alfrescoGroups,
+            List<NucleusGroupOutput> currentNucleusGroups,
+            Map<String, List<String>> userGroupMembershipsCache)
+    {
+        Set<String> relevantAlfrescoGroupIds = userGroupMembershipsCache.values().stream()
+                .filter(groups -> groups != null)
+                .flatMap(List::stream)
+                .collect(Collectors.toSet());
+
+        List<AlfrescoGroup> filteredAlfrescoGroups = alfrescoGroups.stream()
+                .filter(g -> relevantAlfrescoGroupIds.contains(g.id()))
+                .toList();
+
+        Map<String, AlfrescoGroup> alfrescoGroupById = filteredAlfrescoGroups.stream()
+                .collect(Collectors.toMap(AlfrescoGroup::id, Function.identity()));
+
+        Map<String, NucleusGroupOutput> nucleusGroupByAlfrescoId = currentNucleusGroups.stream()
+                .collect(
+                        Collectors.toMap(
+                                NucleusGroupOutput::externalGroupId,
+                                Function.identity()));
+
+        List<NucleusGroupInput> nucleusGroupsToCreate = new ArrayList<>();
+        List<String> nucleusGroupsToDelete = new ArrayList<>();
+        List<GroupMapping> updatedGroupMappings = new ArrayList<>();
+
+        Set<String> allGroupIds = new HashSet<>();
+        allGroupIds.addAll(alfrescoGroupById.keySet());
+        allGroupIds.addAll(nucleusGroupByAlfrescoId.keySet());
+
+        for (String groupId : allGroupIds)
+        {
+            AlfrescoGroup alfrescoGroup = alfrescoGroupById.get(groupId);
+            NucleusGroupOutput nucleusGroup = nucleusGroupByAlfrescoId.get(groupId);
+
+            boolean hasAlfrescoGroup = alfrescoGroup != null;
+            boolean hasNucleusGroup = nucleusGroup != null;
+
+            if (hasAlfrescoGroup)
+            {
+                if (!hasNucleusGroup)
+                {
+                    nucleusGroupsToCreate.add(new NucleusGroupInput(alfrescoGroup.id()));
+                }
+                updatedGroupMappings.add(
+                        new GroupMapping(alfrescoGroup.id(), alfrescoGroup.displayName()));
+            }
+            else
+            {
+                if (hasNucleusGroup)
+                {
+                    nucleusGroupsToDelete.add(groupId);
+                }
+            }
+        }
+
+        executeGroupBatchOperations(nucleusGroupsToCreate, nucleusGroupsToDelete);
+
+        LOGGER.atDebug()
+                .setMessage("Updated group count: {}")
+                .addArgument(updatedGroupMappings.size())
+                .log();
+
+        return updatedGroupMappings;
+    }
+
+    private void executeGroupBatchOperations(
+            List<NucleusGroupInput> nucleusGroupsToCreate, List<String> nucleusGroupsToDelete)
+    {
+        for (String alfrescoGroupId : nucleusGroupsToDelete)
+        {
+            nucleusClient.deleteGroup(alfrescoGroupId);
+        }
+        if (!nucleusGroupsToDelete.isEmpty())
+        {
+            LOGGER.atTrace()
+                    .setMessage("Deleted groups with ID: {}")
+                    .addArgument(nucleusGroupsToDelete.stream().collect(Collectors.joining(",")))
+                    .log();
+        }
+        LOGGER.atDebug()
+                .setMessage("Deleted {} groups from Nucleus.")
+                .addArgument(nucleusGroupsToDelete.size())
+                .log();
+
+        if (!nucleusGroupsToCreate.isEmpty())
+        {
+            nucleusClient.createGroups(nucleusGroupsToCreate);
+            LOGGER.atTrace()
+                    .setMessage("Created groups for ID: {}")
+                    .addArgument(nucleusGroupsToCreate.stream().map(NucleusGroupInput::externalGroupId).collect(Collectors.joining(",")))
+                    .log();
+        }
+        LOGGER.atDebug()
+                .setMessage("Created {} groups in Nucleus.")
+                .addArgument(nucleusGroupsToCreate.size())
+                .log();
+    }
+}
