c22f1593f19bfca1eb64d12f97d4deca5db57439
tbose <tirthankar.bose@hyland.com>
2026-02-25 12:25:47 +0000

    fix pre commit issue

--- live-ingester/src/main/java/org/alfresco/hxi_connector/live_ingester/adapters/messaging/repository/mapper/property/PropertyMappingHelper.java
+++ live-ingester/src/main/java/org/alfresco/hxi_connector/live_ingester/adapters/messaging/repository/mapper/property/PropertyMappingHelper.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco HX Insight Connector
  * %%
- * Copyright (C) 2023 - 2025 Alfresco Software Limited
+ * Copyright (C) 2023 - 2026 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * If the software was purchased under a paid Alfresco license, the terms of
@@ -147,7 +147,8 @@ public class PropertyMappingHelper
                 : eventData.getResourceDeniedAuthorities();
 
         // Return empty if both are empty
-        if (allowAccess.isEmpty() && denyAccess.isEmpty()) {
+        if (allowAccess.isEmpty() && denyAccess.isEmpty())
+        {
             return Optional.empty();
         }
 
